5f3f23b938999c4f791b1063d7f35c41fb7a0bd4
arifaydogan0 <aydoganarif@yahoo.com>
2022-12-16 08:56:01 +0300

    AA

--- Calculator/Calculator/MainPage.xaml.cs
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Calculator
 {
     public partial class MainPage : ContentPage
     {
+
+        string ekran = "";
+        string rakamlar = "0123456789";
+        string islemler = "+-x/";
+
         public MainPage()
         {
             InitializeComponent();
@@ -17,7 +18,44 @@ namespace Calculator
 
         private void entry_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //sonucu hesapla
+            if (e.NewTextValue.Length != 0)
+                return;
 
+            char sonKarakter = e.NewTextValue[e.NewTextValue.Length - 1];
+
+            if (!rakamlar.Contains(sonKarakter) && !islemler.Contains(sonKarakter))
+                entry.Text = e.OldTextValue;
+
+            ekran = entry.Text;
+        }
+
+        private void Topla_Clicked(object sender, EventArgs e)
+        {
+            if (ekran.Length != 0)
+                if (!islemler.Contains(ekran[ekran.Length - 1]))
+                    entry.Text += "+";
+        }
+
+        private void Çıkar_Clicked(object sender, EventArgs e)
+        {
+            if (ekran.Length != 0)
+                if (!islemler.Contains(ekran[ekran.Length - 1]))
+                    entry.Text += "-";
+        }
+
+        private void Çarp_Clicked(object sender, EventArgs e)
+        {
+            if (ekran.Length != 0)
+                if (!islemler.Contains(ekran[ekran.Length - 1]))
+                    entry.Text += "x";
+        }
+
+        private void Böl_Clicked(object sender, EventArgs e)
+        {
+            if (ekran.Length != 0)
+                if (!islemler.Contains(ekran[ekran.Length - 1]))
+                    entry.Text += "/";
         }
     }
 }
